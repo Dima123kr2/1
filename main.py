1c94d736197a342f156f9df0c89dd7b22ffae62f
Dima123kr <krivosheya_da@mail.ru>
2023-12-28 16:26:58 +0300

    1234567

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt
+from random import randint
+
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("UI.ui", self)
+        self.initUI()
+
+    def initUI(self):
+        self.flag = False
+        self.s1 = 1
+        self.s2 = 100000
+        self.s3 = 100000
+        self.btn.clicked.connect(self.start)
+
+    def start(self):
+        self.flag = True
+
+    def paintEvent(self, event):
+            painter = QPainter(self)
+            painter.setPen(QPen(Qt.blue, 8))
+            painter.setBrush(QBrush(Qt.red))
+            if self.flag:
+                self.s1 = randint(10, 50)
+                self.s2 = randint(1, 400)
+                self.s3 = randint(1, 300)
+                self.flag = False
+            painter.drawEllipse(self.s2, self.s3, self.s1, self.s1)
+            painter.end()
+            self.update()
+
+
+if __name__ == '__main__':
+    sa = QApplication(sys.argv)
+    sx = Main()
+    sx.show()
+    sys.exit(sa.exec())
