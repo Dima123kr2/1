6cdbc615fbc1fed626942f02ffdbe9da8c015123
Dima123kr <krivosheya_da@mail.ru>
2023-12-28 16:51:07 +0300

    0123456

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QPainter, QBrush, QPen
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
+from PyQt5 import QtCore, QtWidgets
 from random import randint
+from UI import Ui_Form
 
 
-class Main(QWidget):
+class Main(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
@@ -17,6 +18,7 @@ class Main(QWidget):
         self.s1 = 1
         self.s2 = 100000
         self.s3 = 100000
+        self.sq = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
         self.btn.clicked.connect(self.start)
 
     def start(self):
@@ -24,13 +26,14 @@ class Main(QWidget):
 
     def paintEvent(self, event):
             painter = QPainter(self)
-            painter.setPen(QPen(Qt.red, 8))
-            painter.setBrush(QBrush(Qt.yellow))
             if self.flag:
                 self.s1 = randint(10, 50)
                 self.s2 = randint(1, 400)
                 self.s3 = randint(1, 300)
+                self.sq = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
                 self.flag = False
+            painter.setPen(QPen(self.sq, 8))
+            painter.setBrush(QBrush(self.sq))
             painter.drawEllipse(self.s2, self.s3, self.s1, self.s1)
             painter.end()
             self.update()
